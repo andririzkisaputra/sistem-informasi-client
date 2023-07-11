cef2153d1e427522a80fbea4441ea89611dc150f
andririzkisaputra <andri.rizki007@gmail.com>
2023-07-12 00:57:19 +0700

    db

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,7 +26,7 @@ class Database extends Config
      */
     public array $default = [
         'DSN'      => '',
-        'hostname' => 'mysql',
+        'hostname' => 'mariadb',
         'username' => 'web',
         'password' => 'web',
         'database' => 'repository',
